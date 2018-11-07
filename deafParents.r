eaa017d555c33c6f9ec171262d792f0530bba2c2
adamSales <asales@utexas.edu>
2018-11-07 12:55:19 -0600

    got it all together I think...

--- deafParents.r
+++ deafParents.r
@@ -41,8 +41,8 @@ pdat <- pdat%>%mutate(deafKid= (dear==1)&(agep<19))%>%
 
 pdat <- pdat%>%
     mutate(
-        fakeAge <- age+14,
-        unmarriedPartnerParent=(relp==13)&(agep>supressWarnings(min(fakeAge[deafKid&(relp==15)]))),
+        fakeAge= agep+14,
+        unmarriedPartnerParent=(relp==13)&(agep>suppressWarnings(min(fakeAge[deafKid&(relp==15)]))),
                                         #unmarried partner
                                         #may be parent of deaf kid. logic: at least 15 years older than
                                         # some deaf kid in house who is unrelated to ref.
@@ -60,48 +60,77 @@ pdat <- pdat%>%
             (any(relp%in%c(2:4,14,7))&(relp==0))| #ego is ref; kid or grandkid in HH
             (any(relp%in%c(2:4,7,14))&(relp%in%c(1,13)))| # ego married/partner to ref; ref's (grand)kid in HH
             (relp==6)| # ego is parent of ref
-            (relp%in%c(2:4,9,14)&any(relp==7))| #ego is parent, ref is grandparent
+            (relp%in%c(2:4,9,14)&any(relp==7)&(agep>max(fakeAge[relp==7])))| #ego is parent, ref is grandparent
                                         #(maybe ego is aunt/uncle of kids...impossible to tell I think)
             unmarriedPartnerParent|
             ((relp==0)&any(unmarriedPartnerParent)), # ego's unmarried partner is a parent (?)
-        deafCharge=deafKid&(fakeAge<supressWarnings(max(agep[agep>17]))), # there's at least one adult 15 years
+        deafCharge=deafKid&(fakeAge<suppressWarnings(max(agep[agep>17]))), # there's at least one adult 15 years
                                         #older than this deaf kid
         numDeafKids=sum(deafKid),
         numKids=sum(kid),
         numDeafParents=sum(parent&(dear==1)),
-        anyDeafParents=numDeafParents>0,
-        twoPlusDeafParents=numDeafParents>1,
         numParents=sum(parent))%>%
     ungroup()
 
+## worry: multigenerational HH with young deaf parent(s). middle generation are both "parents" and "kids"
+## someone cannot be their own deaf parent! example:
+## grampa bob (hearing, age 60), bob's daughter alice (deaf, age 18), alice's partner beth (deaf, age 19),
+## alice & beth's son joe (deaf, age 1).
+## number of deaf parents in HH: 2. But alice shouldn't be counted as living with deaf parents--she is the deaf
+## parent!
+## instead, for each person, count number of parents in HH 15 years older then _them_ who are deaf.
+## then joe has two deaf parents, alice has 0 deaf parents. (beth is >18 so we don't count her as kid)
+
+
+countDeafParents <- function(i){
+     rowi <- pdat[i,]
+     hh <- filter(pdat[-i,],serialno==rowi$serialno)
+     with(hh, sum(parent&(agep>rowi$fakeAge)&(dear==1)))
+}
+
+deafkidparentrows <- which(pdat$kid&(pdat$dear==1)&pdat$parent)
+ndp <- vapply(deafkidparentrows,countDeafParents,1)
+
+pdat$numDeafParents[deafkidparentrows] <- ndp
+
+countDeafAdults <- function(deafCharge,adult,agep,fakeAge,dear){
+    out <- rep(NA,length(adult))
+    if(!any(deafCharge)) return(out)
+    dc <- which(deafCharge)
+    cc <- vapply(dc,function(i) sum(adult&(agep> fakeAge[i])&(dear==1)),integer(1))
+    out[dc] <- cc
+    out
+}
+
+pdat <- pdat%>%group_by(serialno)%>%
+    mutate(numDeafAdults=countDeafAdults(deafCharge,adult,agep,fakeAge,dear))%>%
+    ungroup()
+
+
 pdat <- within(pdat,{
-    numAdults=vapply(1:nrow(pdat),function(i) sum(adult&(serialno==serialno[i])&(agep>fakeAge[i])),1)
-    numDeafAdults=vapply(1:nrow(pdat),function(i) sum(adult&(serialno==serialno[i])&(agep>fakeAge[i])&(dear==1)),1)
-})
-pdat <- within(pdat,{
+    anyDeafParents=numDeafParents>0
+    twoPlusDeafParents=numDeafParents>1
     anyDeafAdults=numDeafAdults>0
     twoPlusDeafAdults=numDeafAdults>1
 })
 
+results <- list(
+    anyPar=estExpr(anyDeafParents,kid&(dear==1),pdat),
+    twoPar=estExpr(twoPlusDeafParents,kid&(dear==1),pdat),
+    anyAd=estExpr(anyDeafAdults,deafCharge,pdat),
+    twoAd=estExpr(twoPlusDeafAdults,deafCharge,pdat))
+
+save(pdat,results,file='deafHH.RData')
 
 sink('deafParents.txt')
 print('% deaf kids living w/ parent/guardian with ANY deaf parent/guardian (proportion, SE, n):')
-print(estExpr(anyDeafParents,kid&(dear==1),pdat))
+print(results$anyPar)
 print('% deaf kids living w/ parent/guardian with 2+ deaf parent/guardian (proportion, SE, n):')
-print(estExpr(twoPlusDeafParents,kid&(dear==1),pdat))
+print(results$twoPar)
 
 print('% deaf kids living w/ older adults with ANY deaf adults (proportion, SE, n):')
-print(estExpr(anyDeafAdults,deafCharge,pdat))
+print(results$anyAd)
 print('% deaf kids living w/ older adults with ANY deaf adults (proportion, SE, n):')
-print(estExpr(twoPlusDeafAdults,deafCharge,pdat))
+print(results$twoAd)
 sink()
 
-
-byHH <- pdat%>%group_by(serialno)%>%summarize(
-                                        numDeafKids=mean(numDeafKids),
-                                        numKids=mean(numKids),
-                                        numDeafParents=mean(numDeafParents),
-                                        numParents=mean(numParents),
-                                        numDeafAdults=ifelse(any(deafCharge),mean(numDeafAdults[deafCharge]),0),
-                                        numAdults=ifelse(any(deafCharge),mean(numAdults[deafCharge]),0),
-                                        size=n())

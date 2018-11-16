12170cd3448914ad7d5b228b2046e9aa0e0ba6a1
adamSales <asales@utexas.edu>
2018-11-16 13:27:38 -0600

    final small business commit?

--- smallBusiness.r
+++ smallBusiness.r
@@ -2,6 +2,8 @@ library(readr) ## read in the csvs faster
 library(dplyr)
 library(survey)
 
+source('generalCode/estimationFunctions.r')
+source('generalCode/median.r')
 ## Median Annual Salary STEM
 ## Number/Percent in STEM field
 
@@ -9,7 +11,7 @@ library(survey)
 
 
 
-pVars <- c('SERIALNO','DEAR','ST','AGEP','COW','ADJINC','SCHL','WKW','WKHP','SCH','WAGP', 'PERNP', 'PINCP', 'SSIP', 'OCCP','COW','ESR','PWGTP',paste0('PWGTP',1:80))
+pVars <- c('SERIALNO','DEAR','ST','AGEP','COW','ADJINC','SCHL','WKW','WKHP','SCH','WAGP', 'PERNP', 'PINCP', 'SSIP', 'OCCP','ESR','RELP','PWGTP',paste0('PWGTP',1:80))
 
     #hVars <- c('SERIALNO','TYPE','LNGI','HUPAC','HUPAOC','HUPARC','FPARC','FES','HHT','PARTNER','WKEXREL','WIF','HINCP')
 
@@ -34,32 +36,111 @@ gc()
 
 names(sdat) <- tolower(names(sdat))
 
-sdat$state <- states$abb[match(sdat$ST,states$x)]
-
-sdat$stem <- sdat$occp%in%stemCodes$code
-sdat$stemrel <- sdat$occp%in%c(stemRelatedCodes$code,stemCodes$code)
+### limit to non-institutional, ages 25-64
+sdat <- filter(sdat,relp!=16,agep>24,agep<65)
 
 sdat$adj <- sdat$adjinc/1e6
 
-sdat <- sdat%>%filter(agep>24,agep<65)%>%
-    mutate(hs = schl>=16,
-           ba = schl>=21,
+sdat <- mutate(sdat,
            employed = esr%in%c(1,2,4,5),
            unemployed = esr==3,
            fulltime=(wkw==1 & wkhp>=35),
            earn=pernp*adj,
            inc=pincp*adj,
+           selfEmp=cow%in%(6:7),
+           smallBiz=cow==7
+           )
 
-           raceEth=ifelse(hisp>1,"Hispanic",
-                   ifelse(rac1p==2,"African American",
-                   ifelse(rac1p==6| rac1p==7,"Asian/PacIsl",
-                   ifelse(rac1p%in%c(3,4,5),'American Indian',
-                   ifelse(rac1p==1,"White","Other"))))))
+sdat$cow[is.na(sdat$cow)] <- 0 ## ppl not in labor force. give them separate category
 
-sdat <- sdat%>%filter(fulltime)%>%mutate(stemJob=factor(ifelse(stem,'stem',
-                                                        ifelse(stemrel,'stem-related','non-stem')),
-                                                        levels=c('stem','stem-related','non-stem')),
-                                         BA=factor(ifelse(ba&(sciengp==1),'stem',
-                                                   ifelse(ba&(sciengrlp==1),'stem-related',
-                                                   ifelse(ba,'non-stem','none'))),
-                                                   levels=c('stem','stem-related','non-stem','none')))
+## percent own small biz
+ownSmallBiz <- list(
+    overall= rbind(deaf=estExpr(smallBiz,dear==1,sdat),
+                   hearing=estExpr(smallBiz,dear==2,sdat)),
+    employed=rbind(deaf=estExpr(smallBiz,(dear==1)&employed,sdat),
+                   hearing=estExpr(smallBiz,(dear==2)&employed,sdat)),
+    under40=rbind(deaf=estExpr(smallBiz,(dear==1)&(agep<40),sdat),
+                  hearing=estExpr(smallBiz,(dear==2)&(agep<40),sdat)),
+    under40Employed=rbind(deaf=estExpr(smallBiz,(dear==1)&employed&(agep<40),sdat),
+                   hearing=estExpr(smallBiz,(dear==2)&employed&(agep<40),sdat)))
+
+
+selfEmp <- list(
+    overall= rbind(deaf=estExpr(selfEmp,dear==1,sdat),
+                   hearing=estExpr(selfEmp,dear==2,sdat)),
+    employed=rbind(deaf=estExpr(selfEmp,(dear==1)&employed,sdat),
+                   hearing=estExpr(selfEmp,(dear==2)&employed,sdat)),
+    under40=rbind(deaf=estExpr(selfEmp,(dear==1)&(agep<40),sdat),
+                  hearing=estExpr(selfEmp,(dear==2)&(agep<40),sdat)),
+    under40Employed=rbind(deaf=estExpr(selfEmp,(dear==1)&employed&(agep<40),sdat),
+                   hearing=estExpr(selfEmp,(dear==2)&employed&(agep<40),sdat)))
+
+deafOwners <- list(
+    percentDeaf=estExpr(dear==1,sdat=sdat),
+    numDeaf=svTot(sdat,'dear==1'),
+    percentDeafUnder40=estExpr(dear==1,agep<40,sdat=sdat),
+    numDeafUnder40=svTot(sdat,'(dear==1)&(agep<40)'),
+    percentSB=estExpr(dear==1,smallBiz,sdat),
+    percentSE=estExpr(dear==1,selfEmp,sdat),
+    percentSBunder40=estExpr((dear==1),smallBiz&(agep<40),sdat),
+    percentSEunder40=estExpr((dear==1),selfEmp&(agep<40),sdat),
+    numSB=svTot(sdat,'(dear==1)&smallBiz'),
+    numSE=svTot(sdat,'(dear==1)&selfEmp'),
+    numSBu40=svTot(sdat,'(dear==1)&smallBiz&(agep<40)'),
+    numSEu40=svTot(sdat,'(dear==1)&selfEmp&(agep<40)'))
+
+
+
+medInc <- list(
+ smallBiz=rbind(
+     deaf=medStr('inc',subst='(dear==1)&smallBiz&employed',sdat=sdat),
+     hearing=medStr('inc',subst='(dear==2)&smallBiz&employed',sdat=sdat)),
+ smallBizUnder40=rbind(
+     deaf=medStr('inc',subst='(dear==1)&smallBiz&employed&(agep<40)',sdat=sdat),
+     hearing=medStr('inc',subst='(dear==2)&smallBiz&employed&(agep<40)',sdat=sdat)),
+ selfEmp=rbind(
+     deaf=medStr('inc',subst='(dear==1)&selfEmp&employed',sdat=sdat),
+     hearing=medStr('inc',subst='(dear==2)&selfEmp&employed',sdat=sdat)),
+ selfEmpUnder40=rbind(
+     deaf=medStr('inc',subst='(dear==1)&selfEmp&employed&(agep<40)',sdat=sdat),
+     hearing=medStr('inc',subst='(dear==2)&selfEmp&employed&(agep<40)',sdat=sdat)))
+
+
+save(ownSmallBiz,selfEmp,deafOwners,medInc,file='smallBizResults.RData')
+
+trans <- function(res,cn){
+ res <- lapply(names(res),
+  function(x){
+   if(NCOL(res[[x]])>1) return(
+    rbind(
+     c(x,rep('',ncol(res[[x]])+1)),
+     cbind(rep('',nrow(res[[x]])),rownames(res[[x]]),round(res[[x]],1))
+    )
+   )
+  rbind(c(x,round(res[[x]],1)))
+ })
+
+ mlen <- max(sapply(res,NCOL))
+ fff <- function(x) if(ncol(x)<mlen) cbind(x,matrix('',nrow(x),mlen-ncol(x))) else x
+ res <- lapply(res,fff)
+
+ mat <- do.call('rbind',res)
+ if(!missing(cn))
+     mat <- rbind(c(rep('',ncol(mat)-length(cn)),cn),mat)
+ mat <- rbind(c('ACS 2012-2016',rep('',ncol(mat)-1)),mat)
+ mat <- rbind(mat,
+     c('Ages 25-64 unless otherwise specified; non-institutionalized',
+                rep('',ncol(mat)-1)))
+
+ mat
+}
+
+
+library(openxlsx)
+
+write.xlsx(list(
+    smallBusinessOwners=trans(ownSmallBiz,c('% Own Small Biz','SE','n')),
+    selfEmployed=trans(selfEmp,c('% Self-Employed','SE','n')),
+    deafOwners=trans(deafOwners,c('% or Number','SE','n')),
+    medianIncome=trans(medInc,c('Median Income','SE','n'))),
+    file='SmallBiz.xlsx',row.names=FALSE,col.names=FALSE)
